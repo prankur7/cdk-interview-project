eaad79be53bd4a4d4e0ec19fab39176648b17061
Prankur gupta <prankurgupta.gupta7@gmail.com>
2024-05-07 21:10:22 -0700

    adding cdk project files to repo

--- bin/cdk_project.ts
+++ bin/cdk_project.ts
@@ -5,7 +5,14 @@ import { MyServerLessStack } from '../stack/srvrlss-stack'
 
 // cdk app encapuslating a stack  
 const app = new cdk.App();
-new MyServerLessStack(app, 'MyServerLessStack', {}
+new MyServerLessStack(app, 'MyServerLessStack', {
+    FunctionName: "MyLambdaFucntion",
+    RoleName: "LambdaRole",
+    TableName: "DBTestTable",
+    ReadCapacity: 20,
+    WriteCapacity: 20,
+    RestApiName: "MyRestAPI"
+}
 );
 
 app.synth()
--- stack/srvrlss-stack.ts
+++ stack/srvrlss-stack.ts
@@ -3,18 +3,28 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AWSResourcesConstructt } from '../lib/MyResourceConstructs'
 
+export interface StackProps{
+  FunctionName: string;
+  RoleName: string;
+  TableName: string;
+  ReadCapacity: number;
+  WriteCapacity: number;
+  RestApiName: string
+}
+
+
 // creating a stack using MyResourceConstructs construct  
 export class MyServerLessStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
+    constructor(scope: Construct, id: string, props: StackProps) {
+      super(scope, id);
 
       new AWSResourcesConstructt(this, 'AWSResourcesConstructt', {
-        functionName: "MyLambdaFucntion",
-        roleName: "LambdaRole",
-        tableName: "DBTestTable",
-        readCapacity: 20,
-        writeCapacity: 20,
-        restApiName: "MyRestAPI"
+        functionName: props.FunctionName,
+        roleName: props.RoleName,
+        tableName: props.TableName,
+        readCapacity: props.ReadCapacity,
+        writeCapacity: props.WriteCapacity,
+        restApiName: props.RestApiName
       });
 
 
